765f80b63d7b12fe1136ab3005907e84c8c034d1
Mengtao.Hu <305945915@qq.com>
2016-11-02 00:47:44 +0800

    fix notify bug

--- resources/views/web/component/notify.blade.php
+++ resources/views/web/component/notify.blade.php
@@ -1,9 +1,11 @@
-@if(Auth::check()&&(Auth::user()->unreadNotifications->where('type','App\Notifications\Comments')->count()>0||Auth::user()->unreadNotifications->where('type','App\Notifications\Thumb')->count()>0)||Auth::user()->unreadNotifications->where('type','App\Notifications\Notify')->count()>0 )
-    <div style="position:absolute;right: 2%;top: 2%;" data-toggle="tooltip" data-placement="left"
-         title="{{ Auth::user()->unreadNotifications->where('type','App\Notifications\Comments')->count()||Auth::user()->unreadNotifications->where('type','App\Notifications\Notify')->count()+Auth::user()->unreadNotifications->where('type','App\Notifications\Thumb')->count() }} 条新通知">
-        <a href="/notification"><span class="badge"><span
-                        class="glyphicon glyphicon-bullhorn"></span>{{ Auth::user()->unreadNotifications->where('type','App\Notifications\Comments')->count()+Auth::user()->unreadNotifications->where('type','App\Notifications\Notify')->count()+Auth::user()->unreadNotifications->where('type','App\Notifications\Thumb')->count() }}</span></a>
-    </div>
+@if(Auth::check())
+    @if((Auth::user()->unreadNotifications->where('type','App\Notifications\Comments')->count()>0||Auth::user()->unreadNotifications->where('type','App\Notifications\Thumb')->count()>0)||Auth::user()->unreadNotifications->where('type','App\Notifications\Notify')->count()>0 )
+        <div style="position:absolute;right: 2%;top: 2%;" data-toggle="tooltip" data-placement="left"
+             title="{{ Auth::user()->unreadNotifications->where('type','App\Notifications\Comments')->count()||Auth::user()->unreadNotifications->where('type','App\Notifications\Notify')->count()+Auth::user()->unreadNotifications->where('type','App\Notifications\Thumb')->count() }} 条新通知">
+            <a href="/notification"><span class="badge"><span
+                            class="glyphicon glyphicon-bullhorn"></span>{{ Auth::user()->unreadNotifications->where('type','App\Notifications\Comments')->count()+Auth::user()->unreadNotifications->where('type','App\Notifications\Notify')->count()+Auth::user()->unreadNotifications->where('type','App\Notifications\Thumb')->count() }}</span></a>
+        </div>
+    @endif
 @endif
 
 <script>
