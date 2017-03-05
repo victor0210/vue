a66b7befac6b9f345a092925a17e34cc29e99da6
Mengtao.Hu <305945915@qq.com>
2017-03-05 22:21:23 +0800

    disable userscale on mobile

--- resources/views/web/other-user/app.blade.php
+++ resources/views/web/other-user/app.blade.php
@@ -5,7 +5,7 @@
     <title>@yield('title')</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="viewport" content="initial-scale=1">
+    <meta name="viewport" content="initial-scale=1,user-scalable=no">
     <link rel="stylesheet" href="{{ elixir('assets/css/app.css') }}">
     <link rel="stylesheet" href="{{ elixir('assets/css/zoom.css') }}">
     <link rel="stylesheet" href="{{ elixir('assets/css/navbar.css') }}">
